c960e5d841cf9e70b17fc43011587b76e834e86e
hexermelvin117 <melvinre1997@gmail.com>
2021-01-29 20:00:29 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to my App"/>
+  <TestComp titleMessage="This look pretty cool" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TestComp from './components/TestComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TestComp
   }
 }
 </script>
---
+++ src/components/TestComp.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>{{ titleMessage }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'TestComp',
+  props: {
+    titleMessage: String
+  }
+}
+</script>
